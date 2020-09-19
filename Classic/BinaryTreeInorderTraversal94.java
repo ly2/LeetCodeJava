92b4bf3c100ebfb7cf9d41800766d87f35d5cb3d
Yuyuan Liang <u6319242@anu.edu.au>
2020-09-19 23:07:50 +1000

    add a new file folder classic and new file

---
+++ Classic/BinaryTreeInorderTraversal94.java
@@ -0,0 +1,48 @@
+/*
+        94. Binary Tree Inorder Traversal
+        Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+        Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+
+public class BinaryTreeInorderTraversal94 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+
+        return null;
+    }
+}
