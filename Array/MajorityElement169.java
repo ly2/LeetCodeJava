35e6ff6c4f49b2653c3f6e18a4820f79b7c8f72c
Yuyuan Liang <u6319242@anu.edu.au>
2020-09-20 03:15:00 +1000

    238 add

--- Array/MajorityElement169.java
+++ Array/MajorityElement169.java
@@ -7,9 +7,36 @@ You may assume that the array is non-empty and the majority element always exist
 
 
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MajorityElement169 {
     public int majorityElement(int[] nums) {
 
+        int n = nums.length/2;
+        HashMap<Integer, Integer> counter = new HashMap<Integer, Integer>();
+        for (int i : nums) {
+
+            if (counter.containsKey(i)) {
+                Integer oldValue = counter.get(i);
+                counter.put(i, oldValue+1);
+            } else {
+                counter.put(i, 1);
+            }
+            if (counter.get(i) > n) {
+                return i;
+            }
+
+        }
+        for (Map.Entry<Integer, Integer> pair: counter.entrySet()) {
+            if (pair.getValue() > n)
+                return pair.getKey();
+        }
         return 0;
+
+
+/*        Arrays.sort(nums);
+        return nums[nums.length/2];*/
     }
 }
